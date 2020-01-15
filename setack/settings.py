2960ab62c6095c560723425cbf01708aa75487c0
hyeri <hyeri2565@skku.edu>
2020-01-15 21:33:13 +0900

    revise error of applying css files

--- setack/settings.py
+++ setack/settings.py
@@ -136,7 +136,7 @@ STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'setackapp', 'static')
-,    os.path.join(BASE_DIR,'acount')
-]
+,    os.path.join(BASE_DIR,'account','static')
+    ]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
