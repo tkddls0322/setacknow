07461919d8501066fd787314c52fc62c6adb7ebc
hyeri <hyeri2565@skku.edu>
2020-01-25 00:51:24 +0900

    revise view of account app & setackapp directory (templates/setackapp)

--- account/models.py
+++ account/models.py
@@ -5,6 +5,6 @@ from django.contrib.auth.models import User
 
 class NowUser(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    pn = models.CharField(max_length=20, blank=True)
+    phonenum = models.CharField(max_length=20, blank=True)
 
 # Create your models here.
--- account/views.py
+++ account/views.py
@@ -1,26 +1,41 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.contrib.auth.models import User
 from django.contrib import auth
+from .models import NowUser
 
 # Create your views here.
 def signup(request):
+    context={}
     if request.method =="POST":
-        if request.POST["password1"]==request.POST["password2"]:
-            user=User.objects.create_user(username=request.POST["username"],password=request.POST["password1"])
-            auth.login(request,user)
-            return redirect("index")
-        return render(request, 'signup.html')
+        if request.POST.get("pw1")==request.POST.get("pw2"):
+            try:
+                user = User.objects.create_user(username=request.POST.get('sid'), password=request.POST.get('pw1'))
+
+            except IntegrityError:
+                context.update({"error": "이미 가입된 학번입니다."})
+                return render(request, "signup.html", context)
+
+            now_user = NowUser(user=user)
+
+            now_user.phonenum = request.POST.get("pn")
+
+            now_user.save()
+
+            return render(request,"setackapp/index.html", context)
+
+        else:
+            return render(request, 'signup.html')
 
     return render(request, 'account/signup.html')
 
 def login(request):
     if request.method=="POST":
-        username=request.POST["username"]
-        password=request.POST["password"]
+        username=request.POST["sid"]
+        password=request.POST["pw"]
         user=auth.authenticate(request,username=username,password=password)
         if user is not None:
             auth.login(request,user)
-            return redirect("index")
+            return redirect("laundry.html")
         else:
             return render(request,"loginpage.html",{'error':'username or password is incorrect'})
     else:
